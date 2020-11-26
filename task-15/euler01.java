d5df7aa638972d9d1ef8c4ef15af1c8d61f23393
im45145v <74368820+im45145v@users.noreply.github.com>
2020-11-26 18:33:45 +0530

    Update euler01.java

--- task-15/euler01.java
+++ task-15/euler01.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println(enter u r value:)");
+       
         int temp = 0;
         int temp2 = 0; 
-         System.out.println(enter u r value:)");
+         System.out.println("enter u r value:");
  Scanner sc =new Scanner(System.in);
 		int b = sc.nextInt();
         for (int i = 0; i <= b; i++) {
