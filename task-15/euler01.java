fca09d02d2e6a42ce0d3113a0cb0f9f2ca227574
im45145v <74368820+im45145v@users.noreply.github.com>
2020-11-26 18:35:30 +0530

    Update euler01.java

--- task-15/euler01.java
+++ task-15/euler01.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-       
+       //If we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
+
+Find the sum of all the multiples of  or  below .
+
+
         int temp = 0;
         int temp2 = 0; 
          System.out.println("enter u r value:");
@@ -19,7 +23,7 @@ public class Main
                 temp2 = temp2 + j;
             }
         }
-
+System.out.println("output is : ");
         System.out.println(temp + temp2);
     }
 }
